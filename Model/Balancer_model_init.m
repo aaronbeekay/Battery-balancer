496cc14cc895876f026dd4e028d5e997c9f77c45
Aaron Bonnell-Kangas <abk@aaronbeekay.info>
2016-01-19 18:09:52 -0500

    Fix incorrect cell internal resistance. Add lockout parameter for recently balanced cells.

--- Model/Balancer_model_init.m
+++ Model/Balancer_model_init.m
@@ -8,19 +8,20 @@ PACK_NUM_PARALLEL	=	10;								% number of parallel cells
 
 %% Cell parameters
 Capacity			=	2.5 * PACK_NUM_PARALLEL * 3600;	% Cell capacity, coulomb
-Ri					=	0.025;							% Cell internal resistance, ohm
-Q0_nominal			=	0.6 * 3600;						% Initial charge, coulomb (baseline)
-Q0_variance_max		=	0.5 * 3600;							% Maximum variance of initial charge, coulomb
+Ri					=	0.025 / PACK_NUM_PARALLEL;		% Cell internal resistance, ohm
+Q0_nominal			=	1.9 * 3600;						% Initial charge, coulomb (baseline)
+Q0_variance_max		=	0.5 * 3600;						% Maximum variance of initial charge, coulomb
 Q0_variance			=	rand(1, PACK_SIZE) .* Q0_variance_max - Q0_variance_max/2;
 Q0 = (ones(1, PACK_SIZE)*Q0_nominal + Q0_variance).*PACK_NUM_PARALLEL;
 
-load('Samsung-25R', 'Voc');								% Load OCV curve from file
+load('Samsung-25R', 'Voc', 'R1', 'C1');					% Load OCV curve and cell parameters from file
 
 %% Balancer parameters
 Rbalance			=	0.69;							% Balance resistor value, ohm
 
 %% Charger parameters
-Ibulk				=	10;								% Bulk charge current setpoint, amp
+%Ibulk				=	10;								% Bulk charge current setpoint, amp
+Ibulk				=	0;								% Disable charging
 
 %% Control parameters
 % CHARGE_OFF_SAFETY_MAX_VOLT
@@ -54,11 +55,18 @@ BALANCE_MIN_VOLT_THRESH		= 3.0;
 %	If a cell is not currently balancing, it must be more than
 %	BALANCE_ON_VOLT_TOL volts above the lowest cell in order to begin being
 %	balanced.
-BALANCE_ON_VOLT_TOL			= 0.1;
+BALANCE_ON_VOLT_TOL			= 0.06;
 
 % BALANCE_OFF_VOLT_TOL
 %	Voltage difference threshold required to disable balancing for a cell.
 %	If a cell is currently balancing, it must be within
 %	BALANCE_OFF_VOLT_TOL volts of the minimum cell in order to stop being
 %	balanced.
-BALANCE_OFF_VOLT_TOL		= 0.01;
+%
+%	If this value is negative, a balancing cell will be balanced until it
+%	has a lower voltage (under load) than the minimum non-balancing cell.
+BALANCE_OFF_VOLT_TOL		= -0.04;
+
+% BALANCE_RELAXATION_TIME
+%	document
+BALANCE_RELAXATION_TIME		= 5;
--- Model/Simulate_Balancer.m
+++ Model/Simulate_Balancer.m
@@ -14,6 +14,7 @@ sim( 'Balancer' );
 Vc			=	get(logsout, 'Cell voltages');
 Vc			=	Vc.Values;
 
+
 % Cell SOCs
 SOC			=	get(logsout, 'SOC1');
 SOC			=	SOC.Values;
@@ -30,11 +31,29 @@ balance_command		= balance_command.Values;
 
 %% Plot
 
-figure(1); clf
+% Terminal voltages and balancing target
+figure(1); clf;
+ax1 = subplot(211);
+	% Voltages
 	plot(Vc)
+	ylim([3.2 4.2]);
+	
+ax2 = subplot(212);
+	% Balancing target
+	MinCell			=	Vc;
+	BalMask			=	squeeze(balance_command.Data(1,:,:))'.*100 + 1;
+	MinCell.Data	=	min(MinCell.Data .* BalMask, [], 2);
+	MinCell.Name	=	'Balance target voltage';
+	
+	plot(MinCell, 'LineWidth', 1.5);
+	ylim([3.2 4.2]);
+	clear BalMask;
+	
 
+% State-of-charge
 figure(2); clf; hold on;
 	plot(SOC);
+	ax3 = gca;
 
 	% Overlay indicators when cell is balancing
 	for i = 1:PACK_SIZE
@@ -42,3 +61,19 @@ figure(2); clf; hold on;
 		bal_data	= squeeze(balance_command.Data(1,i,:));
 		plot(SOC.Time(bal_data), soc_data(bal_data), '*k', 'LineWidth', 2 );
 	end
+	
+	clear bal_data soc_data;
+	
+linkaxes([ax1 ax2 ax3], 'x');
+linkaxes([ax1 ax2], 'y');
+
+% Balance power
+Pbal		= Vc;
+vbal		= squeeze(balance_command.Data)' .* Vc.Data;
+Pbal.Data	= (vbal.^2) ./ Rbalance;
+Pbal.Data	= sum(Pbal.Data, 2);
+
+Pbal.Name	= 'Balance power dissipated';
+
+figure(3); clf; hold on
+plot(Pbal);
